10a3ae40e038e1a5db2c3f39f1b59cca027e02f1
hadiabu <hadi.abuhamed@gmail.com>
2026-05-19 20:21:28 +0300

    cors allow BASE_IP and environemnt varable update in docker-compose

--- api/app/config.py
+++ api/app/config.py
@@ -1,12 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+import os
+BASE = os.getenv("VITE_API_URL", "http://localhost:8000")
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     database_url: str = "postgresql+psycopg://tracker:tracker@db:5432/tracker"
     seed_on_start: bool = False
-    cors_origins: list[str] = ["http://localhost:5173","http://13.61.24.112:8000"]
+    cors_origins: list[str] = [BASE, "http://localhost:5173"]
     secret_key: str = "dev-secret-key-change-in-production"
     token_expire_hours: int = 24
 
