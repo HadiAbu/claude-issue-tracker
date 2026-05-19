117ad9782cab517b4aec185fb8baad91d6cded42
hadiabu <hadi.abuhamed@gmail.com>
2026-05-19 21:15:52 +0300

    test

--- api/app/main.py
+++ api/app/main.py
@@ -35,7 +35,7 @@ app = FastAPI(title="Issue Tracker API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=settings.cors_origins.split(","),
     allow_methods=["*"],
     allow_headers=["*"],
 )
